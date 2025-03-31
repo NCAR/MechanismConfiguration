fc8a85921a67d4d5f9c83392eed60323c478c8b0
Matt Dawson <mattdawson@ucar.edu>
2025-03-31 13:22:41 -0700

    update version for release

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ project = 'Mechanism Configuration'
 current_year = datetime.datetime.now().year
 copyright = f"2022-{current_year}, OpenAtmos"
 author = 'OpenAtmos'
-version = '1.0.0'
+version = 'v0.1.0'
 release = version
 
 # -- General configuration ---------------------------------------------------
