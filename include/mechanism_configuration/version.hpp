18391de70960f4ce3de49f3755ceb1bc59f67a61
Kyle Shores <kyle.shores44@gmail.com>
2025-04-08 11:39:02 -0500

    correctly placing species as a reactant in a first order loss reaction (#81)
    * correctly placing species as a reactant in a first order loss reaction
    
    * patch release version

--- include/mechanism_configuration/version.hpp
+++ include/mechanism_configuration/version.hpp
@@ -9,7 +9,7 @@ extern "C" {
 
   const char* getVersionString()
   {
-    return "0.1.0";
+    return "0.1.1";
   }
   unsigned getVersionMajor()
   {
@@ -21,7 +21,7 @@ extern "C" {
   }
   unsigned getVersionPatch()
   {
-    return 0+0;
+    return 1+0;
   }
   unsigned getVersionTweak()
   {
