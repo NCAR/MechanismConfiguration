c1086a46812ce270ad5359e0aacf97a8f7b998ac
Matt Dawson <mattdawson@ucar.edu>
2025-04-30 16:18:26 -0700

    ensure all reaction types can be default constructed

--- mechanism_configuration/tests/test_parser.py
+++ mechanism_configuration/tests/test_parser.py
@@ -659,4 +659,35 @@ def test_hard_coded_full_v1_configuration():
     )
 
     validate_full_v1_mechanism(mechanism)
+
+def test_hard_coded_default_constructed_types():
+    arrhenius = Arrhenius()
+    assert arrhenius.type == ReactionType.Arrhenius
+    condensed_phase_arrhenius = CondensedPhaseArrhenius()
+    assert condensed_phase_arrhenius.type == ReactionType.CondensedPhaseArrhenius
+    condensed_phase_photolysis = CondensedPhasePhotolysis()
+    assert condensed_phase_photolysis.type == ReactionType.CondensedPhasePhotolysis
+    emission = Emission()
+    assert emission.type == ReactionType.Emission
+    first_order_loss = FirstOrderLoss()
+    assert first_order_loss.type == ReactionType.FirstOrderLoss
+    henrys_law = HenrysLaw()
+    assert henrys_law.type == ReactionType.HenrysLaw
+    photolysis = Photolysis()
+    assert photolysis.type == ReactionType.Photolysis
+    simpol_phase_transfer = SimpolPhaseTransfer()
+    assert simpol_phase_transfer.type == ReactionType.SimpolPhaseTransfer
+    surface = Surface()
+    assert surface.type == ReactionType.Surface
+    troe = Troe()
+    assert troe.type == ReactionType.Troe
+    tunneling = Tunneling()
+    assert tunneling.type == ReactionType.Tunneling
+    wet_deposition = WetDeposition()
+    assert wet_deposition.type == ReactionType.WetDeposition
+    branched = Branched()
+    assert branched.type == ReactionType.Branched
+    user_defined = UserDefined()
+    assert user_defined.type == ReactionType.UserDefined
+    
     
