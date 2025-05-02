f00acbb637d1365eb2931fb86e135fed2910e973
Dee Grant <waltergrant@gmail.com>
2025-05-01 20:47:10 -0500

    separate reusable defined-mechanism and validation-function from test file

--- mechanism_configuration/tests/test_serializer.py
+++ mechanism_configuration/tests/test_serializer.py
@@ -1,7 +1,20 @@
-import pytest
+from utils_constants import SUPPORTED_FILE_EXTENSIONS
+from utils_mechanism import mechanism_fully_defined, validate_full_v1_mechanism
 
-def test_something():
-    assert 1 == 1
 
-def test_break():
-    assert 1 == 2
+def test_serialize():
+    # TODO: 
+    # use mechanism_fully_defined
+    # export a file for each supported file extension
+    print()
+    for extension in SUPPORTED_FILE_EXTENSIONS:
+        path = f"examples/_missing_configuration{extension}"
+        print(path)
+
+    # import each file
+    # convent import into python object
+    # test newly imported object is equal to mechanism_fully_defined
+    validate_full_v1_mechanism(mechanism_fully_defined) # change to mechanism from imported files
+
+
+# TODO: shoud serializer throw errors/exceptions? what errors should it throw?
---
+++ mechanism_configuration/tests/utils_constants.py
@@ -0,0 +1,1 @@
+SUPPORTED_FILE_EXTENSIONS = [".yaml", ".json"]
