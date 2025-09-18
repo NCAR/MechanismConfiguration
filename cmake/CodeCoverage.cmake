61432496f5f1bb38212bd5605f6956a98728d8d6
Jiwon Gim <jiwongim@ucar.edu>
2025-09-18 14:42:10 -0600

    code cleanup

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -264,32 +264,27 @@ function(setup_target_for_coverage_lcov)
         -b ${BASEDIR} --zerocounters
     )
     # Create baseline to make sure untouched files show up in the report
-    # set(LCOV_BASELINE_CMD 
-    #     ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -c -i -d . -b 
-    #     ${BASEDIR} -o ${Coverage_NAME}.base
-    # )
-    # Create baseline to make sure untouched files show up in the report
     set(LCOV_BASELINE_CMD 
         ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -c -i -d . -b 
         ${BASEDIR} -o ${Coverage_NAME}.base
     )
-
+    # Run tests
     set(LCOV_EXEC_TESTS_CMD 
         ${Coverage_EXECUTABLE} ${Coverage_EXECUTABLE_ARGS}
     )    
     # Capturing lcov counters and generating report
     set(LCOV_CAPTURE_CMD 
-        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors mismatch --gcov-tool ${GCOV_PATH} --directory . -b 
+        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --directory . -b 
         ${BASEDIR} --capture --output-file ${Coverage_NAME}.capture
     )
     # add baseline counters
     set(LCOV_BASELINE_COUNT_CMD
-        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors mismatch --gcov-tool ${GCOV_PATH} -a ${Coverage_NAME}.base 
+        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} -a ${Coverage_NAME}.base 
         -a ${Coverage_NAME}.capture --output-file ${Coverage_NAME}.total
     ) 
     # filter collected data to final coverage report
     set(LCOV_FILTER_CMD 
-        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --ignore-errors mismatch --gcov-tool ${GCOV_PATH} --remove 
+        ${LCOV_PATH} ${Coverage_LCOV_ARGS} --gcov-tool ${GCOV_PATH} --remove 
         ${Coverage_NAME}.total ${LCOV_EXCLUDES} --output-file ${Coverage_NAME}.info
     )    
     # Generate HTML output
