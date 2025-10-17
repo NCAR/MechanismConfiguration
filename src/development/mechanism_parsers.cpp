65c5aa7c8355a3bc2448bda485f4a6edb1acd308
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2025-10-17 11:53:19 -0600

    Centralize validation for Emissions (#181)
    * update config
    
    * add validation
    
    * move file
    
    * validation function for branched reaction
    
    * use error location struct for logging error location
    
    * update the comment
    
    * fix the error message
    
    * refactor emission parser
    
    * correct the error message

--- src/development/mechanism_parsers.cpp
+++ src/development/mechanism_parsers.cpp
@@ -164,7 +164,8 @@ namespace mechanism_configuration
         {
           // TODO - This is temporary until the decoupling is complete
           if (type == validation::Arrhenius_key ||
-              type == validation::Branched_key)
+              type == validation::Branched_key  ||
+              type == validation::Emission_key)
           {
             it->second->Parse(object, reactions);
           }
