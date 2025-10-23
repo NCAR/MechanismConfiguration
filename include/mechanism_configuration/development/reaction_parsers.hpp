5b1402c64a28a7b3a261e6d2857f984f53cf6377
Jiwon Gim <jiwongim@ucar.edu>
2025-10-22 21:35:19 -0600

    add web deposition validator

--- include/mechanism_configuration/development/reaction_parsers.hpp
+++ include/mechanism_configuration/development/reaction_parsers.hpp
@@ -146,12 +146,12 @@ namespace mechanism_configuration
     class WetDepositionParser : public IReactionParser
     {
      public:
-      /// @brief Parser for wet deposition reactions
-      Errors parse(
+      Errors Validate(
           const YAML::Node& object,
           const std::vector<types::Species>& existing_species,
-          const std::vector<types::Phase>& existing_phases,
-          types::Reactions& reactions) override;
+          const std::vector<types::Phase>& existing_phases) override;
+
+      void Parse(const YAML::Node& object, types::Reactions& reactions) override;
     };
 
     class HenrysLawParser : public IReactionParser
