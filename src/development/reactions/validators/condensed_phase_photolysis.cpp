d6ce31357a42e5b9e7e3ac7b55498d25391b4043
Jiwon Gim <55209567+boulderdaze@users.noreply.github.com>
2025-10-24 11:57:52 -0600

    Centralize validation for Condensed-phase photolysis reactions (#219)
    * add talyor series reaction
    
    * fix the comment
    
    * add troe validation
    
    * fix a comment
    
    * add tunneling validator
    
    * fix the test
    
    * add user defined reaction validator
    
    * add ternary reaction validator
    
    * fix a typo
    
    * fix configuration
    
    * condesned arrhenius
    
    * add condensed arrhenius
    
    * fix config
    
    * condensed phase photolysis
    
    * code clean up

---
+++ src/development/reactions/validators/condensed_phase_photolysis.cpp
@@ -0,0 +1,120 @@
+// Copyright (C) 2023–2025 University Corporation for Atmospheric Research
+//                         University of Illinois at Urbana-Champaign
+// SPDX-License-Identifier: Apache-2.0
+
+#include <mechanism_configuration/development/mechanism_parsers.hpp>
+#include <mechanism_configuration/development/reaction_parsers.hpp>
+#include <mechanism_configuration/development/reaction_types.hpp>
+#include <mechanism_configuration/development/utils.hpp>
+#include <mechanism_configuration/development/validator.hpp>
+#include <mechanism_configuration/error_location.hpp>
+#include <mechanism_configuration/validate_schema.hpp>
+
+#include <format>
+
+namespace mechanism_configuration
+{
+  namespace development
+  {
+    /// @brief Validates a YAML-defined Condensed-phase Photolysis reaction entry.
+    ///        Performs schema validation, ensures all referenced species and phases exist,
+    ///        and collects any errors found.
+    /// @param object The YAML node representing the reaction
+    /// @param existing_species The list of known species used for validation
+    /// @param existing_phases The list of known phases used for validation
+    /// @return A list of validation errors, if any
+    Errors CondensedPhasePhotolysisParser::Validate(
+        const YAML::Node& object,
+        const std::vector<types::Species>& existing_species,
+        const std::vector<types::Phase>& existing_phases)
+    {
+      std::vector<std::string> required_keys = {
+        validation::reactants, validation::products, validation::type, validation::condensed_phase
+      };
+      std::vector<std::string> optional_keys = { validation::name, validation::scaling_factor };
+
+      Errors errors;
+
+      auto validation_errors = ValidateSchema(object, required_keys, optional_keys);
+      if (!validation_errors.empty())
+      {
+        errors.insert(errors.end(), validation_errors.begin(), validation_errors.end());
+        return errors;
+      }
+
+      bool is_valid = true;
+
+      // Reactants
+      validation_errors = ValidateReactantsOrProducts(object[validation::reactants]);
+      if (!validation_errors.empty())
+      {
+        errors.insert(errors.end(), validation_errors.begin(), validation_errors.end());
+        is_valid = false;
+      }
+
+      // Products
+      validation_errors = ValidateReactantsOrProducts(object[validation::products]);
+      if (!validation_errors.empty())
+      {
+        errors.insert(errors.end(), validation_errors.begin(), validation_errors.end());
+        is_valid = false;
+      }
+
+      if (!is_valid)
+        return errors;
+
+      std::vector<std::pair<types::ReactionComponent, YAML::Node>> species_node_pairs;
+
+      for (const auto& obj : object[validation::reactants])
+      {
+        types::ReactionComponent component;
+        component.name = obj[validation::name].as<std::string>();
+        species_node_pairs.emplace_back(component, obj);
+      }
+
+      // Validates the number of reactants
+      // This must be done before collecting errors from the products
+      if (species_node_pairs.size() > 1)
+      {
+        const auto& node = object[validation::reactants];
+        ErrorLocation error_location{ node.Mark().line, node.Mark().column };
+
+        std::string message = std::format(
+            "{} error: '{}' reaction requires one reactant, but {} were provided.",
+            error_location,
+            object[validation::type].as<std::string>(),
+            species_node_pairs.size());
+
+        errors.push_back({ ConfigParseStatus::TooManyReactionComponents, message });
+      }
+
+      for (const auto& obj : object[validation::products])
+      {
+        types::ReactionComponent component;
+        component.name = obj[validation::name].as<std::string>();
+        species_node_pairs.emplace_back(component, obj);
+      }
+
+      // Check for unknown species in reactants and products
+      std::vector<NodeInfo> unknown_species = FindUnknownObjectsByName(existing_species, species_node_pairs);
+      if (!unknown_species.empty())
+      {
+        ReportUnknownSpecies(object, unknown_species, errors, ConfigParseStatus::ReactionRequiresUnknownSpecies);
+      }
+
+      // Check for phase existence and get phase reference
+      auto phase_optional = CheckPhaseExists(object, validation::condensed_phase, existing_phases, errors);
+      if (!phase_optional)
+      {
+        return errors;
+      }
+
+      // Check if phase-specific species in reaction is found in phase
+      const auto& phase = phase_optional->get();
+      CheckSpeciesPresenceInPhase(object, phase, species_node_pairs, errors);
+
+      return errors;
+    }
+
+  }  // namespace development
+}  // namespace mechanism_configuration
